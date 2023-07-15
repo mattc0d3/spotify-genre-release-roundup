b5f46f639a9413478b1bf2abf753afec1bdb180d
Matt <matthew.sheehan94@gmail.com>
2023-07-15 11:03:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const clientId = "edfa486ea0f54d70b6ea529eb691ba57"
+const clientSecret = "92c2ffda5c5a40178f1cd79aa13deb12"
+
+const query = {
+    client_id: clientId,
+    redirect_uri: "https://localhost:9090"
+}
